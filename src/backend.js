85d4ddde31161e30de1c413e146d7c5683e6932b
Marqis Ramos <atair01@yahoo.com>
2019-07-05 16:27:53 -0700

    got api working

---
+++ src/backend.js
@@ -0,0 +1,30 @@
+export class doctorSearch {
+  searchBySymptom(symptom,gender,specialty) {
+    if (gender && symptom) {
+      return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=wa-seattle&user_location=47%2C122&gender=${gender}&sort=rating-desc&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        };
+        request.open("GET", url, true);
+        request.send();
+      });
+    } else if (symptom){ 
+      return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=wa-seattle&user_location=47%2C122&sort=rating-desc&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        };
+        request.open("GET", url, true);
+        request.send();
+});
--- src/main.js
+++ src/main.js
@@ -2,41 +2,43 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import from './../src/backend.js'
+import {doctorSearch} from './../src/backend.js'
 
 $(document).ready(function(){
     $('.Search').submit(function(event){
-    event.preventDefault();
-    $('.sucess').text(" ");
-    let symptomsinput = $("#symptoms").val();
-
-
-    let doctorGet = new Promise(function(resolve, reject){
-
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5155%2C-122.413%2C100&user_location=45.5155%C2%B0%2C-122.6793&skip=0&limit=10&user_key=bb9526bb3e6527baac3dce23fefe3a1f`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    doctorGet.then(function(response){
-      let result = JSON.parse(response);
-      $(".sucess").append(`Some physicians in your area are ${doctors} <br>`);
-      for (let i = 0; i < 10; i++) {
-        let doc = result.message.body.city[i].doctor.doctor_name
-        $('.sucess').append(`<li id="results${i}"> ${doctor} </li>`);
-        if (i < 11) {
-
-        }
-      }
-    }
-      function(error) {
-      $('.fail').text(`There was an error processing your request ${error.message}`);
-    });
+      event.preventDefault();
+     let symptom = $('#symptom').val();
+     let gender = $('#gender').val();
+     let search = new DoctorSearch;
+     if (!symptom) {
+       alert("Please enter your symtpoms");
+       return;
+     }
+     $('.error').hide();
+     $('.results').show();
+     $('.results').text("");
+     let results = search.searchBySymptom(symptom,gender);
+     results.then (function(response){
+       let body = JSON.parse(response);
+       if (body.data.length > 0) {
+         $('#docs').text(body.data.length);
+         for (let i = 0; i < body.data.length; i++) {
+           let patients;
+           let name = `${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}`;
+           let phone = body.data[i].practices[0].phones[0].number;
+           let address =` ${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.street2}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}, ${body.data[i].practices[0].visit_address.zip}`;
+           if(body.data[i].practices[0].accepts_new_patients === true) {
+             patients = "Yes";
+           } else {
+             patients = "No";
+           }
+           $('.results').append(`<div class= 'doc${i}'>${name}<br>`);
+           $('.results').append(`<img src="${pic}">`);
+           $('.results').append(`<div class = 'bio'>Phone: ${phone},<br>Address: ${address} <br>Accepting Patients: ${patients} </div></div><hr>`);
+         }
+       } else {
+         $('.error').show();
+       }
+     });
+   });
+ });
