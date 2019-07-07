209d10c511fbc2c99dfc4910334090748453a16b
Marqis Ramos <atair01@yahoo.com>
2019-07-07 11:45:57 -0700

    tried fixing

--- src/backend.js
+++ src/backend.js
@@ -1,11 +1,11 @@
 export class DocSearch {
 
-  searchGenderSymtoms(symptom,gender) {
-    if (gender && symptom) {
+  searchGenderSymtoms(symptoms) {
+    if (symptoms) {
       return new Promise(function(resolve, reject){
         let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=wa-seattle&user_location=47%2C122&gender=${gender}&sort=rating-desc&skip=0&limit=10&user_key=${process.env.API_KEY}`;
-        request.onload = function() {
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=wa-seattle&user_location=47%2C122&sort=rating-desc&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+        request.onload = function(){
           if (this.status === 200) {
             resolve(request.response);
           } else {
@@ -15,20 +15,6 @@ export class DocSearch {
         request.open("GET", url, true);
         request.send();
       });
-    } else if (symptom){
-      return new Promise(function(resolve, reject){
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=wa-seattle&user_location=47%2C122&sort=rating-desc&skip=0&limit=10&user_key=${process.env.API_KEY}`;
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.statusText));
-          }
-        };
-        request.open("GET", url, true);
-        request.send();
-});
-}
-}
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -2,43 +2,42 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {DocSearch} from './../src/backend.js'
+import {DocSearch} from './backend.js'
 
 
 $(document).ready(function(){
     $('.mainP').submit(function(event){
       event.preventDefault();
-     let symptom = $('#symptom').val();
-     let gender = $('#gender').val();
+     let symptoms = $('#symptoms').val();
      let search = new DocSearch;
-     if (!symptom) {
+     if (!symptoms) {
        alert("Please enter your symptoms");
        return;
      }
      $('.error').hide();
      $('.results').show();
      $('.results').text("");
-     let results = search.searchGenderSymtoms(symptom,gender);
+     let results = search.searchGenderSymtoms(symptoms);
      results.then (function(response){
        let body = JSON.parse(response);
        if (body.data.length > 0) {
          $('#showResults').text(body.data.length);
          for (let i = 0; i < body.data.length; i++) {
-           let patients;
+           let newPatient = [];
            let name = `${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}`;
            let phone = body.data[i].practices[0].phones[0].number;
            let address =` ${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.street2}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}, ${body.data[i].practices[0].visit_address.zip}`;
            if(body.data[i].practices[0].accepts_new_patients === true) {
-             patients = "Yes";
+             newPatients = "Yes";
            } else {
-             patients = "No";
+             newPatients = "No";
            }
-           $('.results').append(`<div class= 'doc${i}'>${name}<br>`);
-           $('.results').append(`<div class = 'bio'>Phone: ${phone},<br>Address: ${address} <br>Accepting Patients: ${patients} </div></div><hr>`);
+           $('#showResults').append(`<div class= 'doc${i}'>${name}<br>`);
+           $('#showResults').append(`<div class = 'bio'>Phone: ${phone},<br>Address: ${address} <br>Accepting Patients: ${newpatients} </div></div><hr>`);
          }
        } else {
          $('.error').show();
-       };
+       }
      });
    });
  });
